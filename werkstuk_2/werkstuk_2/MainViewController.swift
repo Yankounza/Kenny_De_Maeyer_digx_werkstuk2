e130f3b4507cddf31ae1502640f59577d8ff443a
DE MAEYER Kenny (s) <kenny.de.maeyer@dt-c205-20.local>
2018-05-24 12:33:39 +0200

    get JSON data with annotations part 1

--- werkstuk_2/werkstuk_2/MainViewController.swift
+++ werkstuk_2/werkstuk_2/MainViewController.swift
@@ -7,18 +7,101 @@
 //
 
 import UIKit
+import MapKit
 
 class MainViewController: UIViewController {
 
+    let URL_VILLO = "https://api.jcdecaux.com/vls/v1/stations?apiKey=6d5071ed0d0b3b68462ad73df43fd9e5479b03d6&contract=Bruxelles-Capitale"
+    var stationArray: Array<Station> = Array()
+    var locationManager: CLLocationManager!
+    var currentLocation: CLLocation!
+    
+    @IBOutlet weak var lastUpdate: UILabel!
+    @IBOutlet weak var stationMap: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        getDataFromUrl();
+        
+        if(CLLocationManager.locationServicesEnabled()) {
+            self.locationManager = CLLocationManager()
+            self.locationManager.delegate = self as? CLLocationManagerDelegate
+            self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            self.locationManager.requestAlwaysAuthorization()
+            self.locationManager.requestWhenInUseAuthorization()
+            self.locationManager.startUpdatingLocation()
+            currentLocation = locationManager.location
+            if(currentLocation != nil) {
+                let radius: CLLocationDistance = 50000
+                let center = CLLocationCoordinate2D(latitude: self.currentLocation.coordinate.latitude, longitude: self.currentLocation.coordinate.longitude)
+                let region = MKCoordinateRegionMakeWithDistance(center, radius, radius)
+                self.stationMap.setRegion(region, animated: true)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func getDataFromUrl() {
+        let url = NSURL(string: URL_VILLO)
+        
+        URLSession.shared.dataTask(with: (url as URL?)!, completionHandler: {(data, response, error) -> Void in
+            if let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSArray {
+                for station in jsonObj! {
+                    let tempObj = Station()
+                    if let stationDict = station as? NSDictionary {
+                        if let obj = stationDict.value(forKey: "number") {
+                            tempObj.number = obj as! Int
+                        }
+                        if let obj = stationDict.value(forKey: "name") {
+                            tempObj.name = obj as! String
+                        }
+                        if let obj = stationDict.value(forKey: "address") {
+                            tempObj.address = obj as! String
+                        }
+                        if let obj = stationDict.value(forKey: "Posiion") {
+                            let pos = obj as! NSArray
+                            tempObj.position.latitude = pos[0] as? Double
+                            tempObj.position.longitude = pos[1] as? Double
+                        }
+                        if let obj = stationDict.value(forKey: "banking") {
+                            tempObj.banking = obj as! Bool
+                        }
+                        if let obj = stationDict.value(forKey: "bonus") {
+                            tempObj.bonus = obj as! Bool
+                        }
+                        if let obj = stationDict.value(forKey: "bike_stands") {
+                            tempObj.total_bike = obj as! Int
+                        }
+                        if let obj = stationDict.value(forKey: "available_bike_stands") {
+                            tempObj.available_stands = obj as! Int
+                        }
+                        if let obj = stationDict.value(forKey: "available_bikes") {
+                            tempObj.available_bikes = obj as! Int
+                        }
+                    }
+                }
+                
+                OperationQueue.main.addOperation ({
+                    self.addStationAnnotations()
+                })
+            }
+            
+        }).resume()
+    }
+    
+    func addStationAnnotations() {
+        for station in stationArray {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: station.position.latitude!, longitude: station.position.longitude!)
+            self.stationMap.addAnnotation(annotation)
+        }
     }
     
 
---
+++ werkstuk_2/werkstuk_2/Station.swift
@@ -0,0 +1,41 @@
+//
+//  Station.swift
+//  werkstuk_2
+//
+//  Created by DE MAEYER Kenny (s) on 24/05/2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+
+struct Position {
+    var latitude:Double?
+    var longitude:Double?
+}
+
+class Station: NSObject {
+    
+    var number:Int
+    var name:String
+    var address:String
+    var position:Position
+    var banking:Bool
+    var bonus:Bool
+    var status:String
+    var total_bike:Int
+    var available_stands:Int
+    var available_bikes:Int
+    
+    override init() {
+        self.number = 0
+        self.name = "//"
+        self.address = "//"
+        self.position = Position(latitude: 0.0, longitude: 0.0)
+        self.banking = false
+        self.bonus = false
+        self.status = "UNKNOWN"
+        self.total_bike = 0
+        self.available_stands = 0
+        self.available_bikes = 0
+    }
+}
